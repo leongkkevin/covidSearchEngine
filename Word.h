dccda3efd2e16f35402267d78207e5e82ced34b3
Kevin Leong <60244334+leongkkevin@users.noreply.github.com>
2020-11-17 18:07:53 -0600

    Update Word.h

--- Word.h
+++ Word.h
@@ -2,13 +2,24 @@
 // Created by Kevin Leong on 11/15/20.
 //
 
+#include "DSHashTable.h"
+
+using namespace std;
+
 #ifndef SEARCH_ENGINE_WORD_H
 #define SEARCH_ENGINE_WORD_H
 
 
 class Word {
+private:
+    string word;
+    DSHashTable<int, string> titleList;
+public:
 
+    string getWord();
+    DSHashTable<int, string> getWordList();
 };
 
 
 #endif //SEARCH_ENGINE_WORD_H
+
