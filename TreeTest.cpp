b38845163c4df4df7a6926f7e30d2ae40c8cff7b
Kevin Leong <60244334+leongkkevin@users.noreply.github.com>
2020-11-17 18:09:02 -0600

    Update TreeTest.cpp

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -2,3 +2,20 @@
 // Created by Kevin Leong on 11/15/20.
 //
 
+#include "catch.hpp"
+#include "DSTree.h"
+#include "Word.h"
+
+TEST_CASE("Insert"){
+    DSTree<int> intTree;
+    intTree.insert(5);
+    REQUIRE(intTree.getNumNodes() == 1);
+    intTree.insert(4);
+    intTree.insert(1);
+    intTree.insert(7);
+    REQUIRE(intTree.get(5)->getHeight() == 1);
+    intTree.get(2);
+
+    DSTree<Word> wordTree;
+
+}
