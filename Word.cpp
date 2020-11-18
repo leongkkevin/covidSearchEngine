ca457604cc2572a812e83677cc603ffdf9a9974c
kkleongsmu <kkleong@smu.edu>
2020-11-18 14:29:24 -0600

    No no big file >:(

--- Word.cpp
+++ Word.cpp
@@ -4,6 +4,28 @@
 
 #include "Word.h"
 
+Word::Word(string word) {
+    this->word = word;
+}
+Word::Word(char *word) {
+    this->word = word;
+}
+Word::Word(const Word &copy) {
+    this->word = copy.word;
+    //this->titleList = copy.titleList;
+}
+
+bool Word::operator>(const Word &comp) const {
+    return this->word > comp.word;
+}
+bool Word::operator<(const Word &comp) const {
+    return this->word < comp.word;
+}
+bool Word::operator==(const Word &comp) const {
+    return this->word == comp.word;
+}
+
+
 string Word::getWord(){
     return this->word;
 }
@@ -11,4 +33,3 @@ string Word::getWord(){
 DSHashTable<int, string> Word::getWordList(){
     return this->titleList;
 }
-
