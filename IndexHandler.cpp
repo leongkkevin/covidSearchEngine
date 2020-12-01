52097ef8a03cddc9eb05beaf26197e94f78e08d6
Sam Timmins <stimmins@smu.edu>
2020-11-30 23:30:28 -0500

    kewl

--- IndexHandler.cpp
+++ IndexHandler.cpp
@@ -10,25 +10,33 @@ void wordSearch(DSTree<Word>& wordIndex, string& search, map<string, int>& found
     toLower(search);
     Word found, word(search);
 
-    if(wordIndex.find(word))
+    if(wordIndex.getNumNodes() > 0)
     {
-        found = wordIndex.get(word);
-
-        map<string, int>::iterator it;
-        int x = 0;
-        for ( it = found.getTitleList().begin(); it != found.getTitleList().end(); it++)
+        if(wordIndex.find(word))
         {
-             //change this to insert into avl tree after demo
-             foundTitles.insert (std::pair<string,int>(it->first,it->second));
-             //cout << it->first << endl;
-             x++;
+            found = wordIndex.get(word);
+
+            map<string, int>::iterator it;
+            int x = 0;
+            for ( it = found.getTitleList().begin(); it != found.getTitleList().end(); it++)
+            {
+                //change this to insert into avl tree after demo
+                foundTitles.insert (std::pair<string,int>(it->first,it->second));
+                //cout << it->first << endl;
+                x++;
+            }
+            //cout << x << " files" << endl;
         }
-        //cout << x << " files" << endl;
+        else
+        {
+            cout << "No titles found for this word" << endl;
+        }
     }
     else
     {
-        cout << "No titles found for this word" << endl;
+        cout << "No index opened, please open an index to search." << endl;
     }
+
 }
 
 void authorSearch(DSHashTable<string, Title>& authorIndex, string& search, map<string, int>& foundTitles)
